4ab97e6f8fd66e0e3a05d4f0ba2f022f2a6a4f81
Soumya-Ranjan-Singh <soumyars675@gmail.com>
2022-10-20 17:50:49 +0530

    Added : Display Welcome to Line Comparison Computation Program

---
+++ src/com/line/comparison/LineComparisonComputation.java
@@ -0,0 +1,9 @@
+//This program is to display the Welcome message to Line Comparison Computation Program.
+
+package com.line.comparison;
+
+public class LineComparisonComputation {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Line Comparison Computation Program");
+    }
+}
